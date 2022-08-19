22efe06a331fbd596f9377bf3848b5fe91b3a88f
Abdullah Shehu <bigtulahi1@gmail.com>
2022-08-19 18:32:54 +0100

    stupid ports

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const http = require('http');
+const express = require('express');
 const websocketServer = require('websocket').server;
+
+const PORT = process.env.PORT || 8080;
+const INDEX = '/index.html';
 const httpServer = http.createServer();
-httpServer.listen(8000, () => console.log('Listening.. on 8000'));
+httpServer.listen(PORT, () => console.log(`Listening.. on ${PORT}`));
 //hashmap clients
 const clients = {};
 const games = {};
@@ -10,112 +14,143 @@ const wsServer = new websocketServer({
   httpServer: httpServer,
 });
 
-wsServer.on('request', (request) => {
-  const connection = request.accept(null, request.origin);
-  const clientId = guid();
-  clients[clientId] = { connection: connection };
-  const payLoad = {
-    method: 'connect',
-    clientId: clientId,
-  };
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-  //send back the client connect
+const connect = () =>
+  wsServer.on('request', (request) => {
+    const connection = request.accept(null, request.origin);
+    const clientId = guid();
+    clients[clientId] = { connection: connection };
+    const payLoad = {
+      method: 'connect',
+      clientId: clientId,
+    };
 
-  //connect
-  connection.on('open', () => console.log('opened!'));
-  connection.on('close', () => console.log('closed!'));
-  connection.on('message', (message) => {
-    const result = JSON.parse(message.utf8Data);
-    if (result.method === 'connected') {
-      const clientId = result.clientId;
-      clients[clientId].userName = result.userName;
-      let list = [];
-
-      for (var key in clients) {
-        if (clients.hasOwnProperty(key)) {
-          if (!clients[key].userName) {
-            delete clients[key];
+    connection.on('open', () => console.log('opened!'));
+    connection.on('close', (e) => {
+      console.log(
+        'Socket is closed. Reconnect will be attempted in 1 second.',
+        e.reason
+      );
+    });
+    connection.on('message', (message) => {
+      const result = JSON.parse(message.utf8Data);
+      if (result.method === 'connected') {
+        const clientId = result.clientId;
+        clients[clientId].userName = result.userName;
+        let list = [];
+        for (var key in clients) {
+          if (clients.hasOwnProperty(key)) {
+            if (!clients[key].userName) {
+              delete clients[key];
+            }
           }
         }
-      }
-      for (var key in clients) {
-        if (clients.hasOwnProperty(key)) {
-          list.push(clients[key].userName);
+        for (var key in clients) {
+          if (clients.hasOwnProperty(key)) {
+            const wow = {
+              userName: clients[key].userName,
+              iD: key,
+            };
+            list.push(wow);
+          }
+        }
+
+        const payLoad = {
+          method: 'online',
+          clientId: clientId,
+          lobby: list,
+        };
+        // const con = clients[clientId].connection;
+        // con.send(JSON.stringify(payLoad));
+
+        for (key in clients) {
+          clients[key].connection.send(JSON.stringify(payLoad));
         }
       }
+      if (result.method === 'disconnected') {
+        const clientId = result.clientId;
+        console.log(clientId);
+        delete clients[clientId];
+      }
+      //I have received a message from the client
+      //a user want to create a new game
+      if (result.method === 'createGame') {
+        const clientId = result.clientId;
+        const gameId = guid();
+        games[gameId] = {
+          id: gameId,
+          clients: [result.clientId, result.oppId],
+        };
 
-      const payLoad = {
-        method: 'online',
-        clientId: clientId,
-        lobby: list,
-      };
-      const con = clients[clientId].connection;
-      con.send(JSON.stringify(payLoad));
-    }
-    //I have received a message from the client
-    //a user want to create a new game
-    // if (result.method === 'create') {
-    //   const clientId = result.clientId;
+        const payLoad = {
+          method: 'inviteGame',
+          clientId: result.oppId,
+          oppName: result.clientName,
+          oppId: clientId,
+          game: games[gameId],
+        };
+        console.log(result);
+        console.log(clients[clientId].userName, clients[result.oppId].userName);
+        const con = clients[result.oppId].connection;
+        con.send(JSON.stringify(payLoad));
+      }
 
-    //   const gameId = guid();
-    //   games[gameId] = {
-    //     id: gameId,
-    //     clients: [],
-    //   };
+      //a client want to join
+      // if (result.method === 'join') {
+      //   const clientId = result.clientId;
+      //   const userName = result.userName;
 
-    //   const payLoad = {
-    //     method: 'create',
-    //     game: games[gameId],
-    //   };
+      //   const gameId = result.gameId;
+      //   const game = games[gameId];
+      //   if (game.clients.length >= 3) {
+      //     //sorry max players reach
+      //     return;
+      //   }
+      //   // const color = { 0: 'Red', 1: 'Green', 2: 'Blue' }[game.clients.length];
+      //   game.clients.push({
+      //     clientId: clientId,
+      //     userName: userName,
+      //     score: 0,
+      //   });
+      //   //start the game
+      //   if (game.clients.length === 3) updateGameState();
 
-    //   const con = clients[clientId].connection;
-    //   con.send(JSON.stringify(payLoad));
-    // }
+      //   const payLoad = {
+      //     method: 'join',
+      //     game: game,
+      //   };
+      //   //loop through all clients and tell them that people has joined
+      //   game.clients.forEach((c) => {
+      //     clients[c.clientId].connection.send(JSON.stringify(payLoad));
+      //   });
+      // }
+      //a user plays
+      // if (result.method === 'play') {
+      //   const gameId = result.gameId;
+      //   // const ballId = result.ballId;
+      //   const score = result.score;
+      //   let state = games[gameId].state;
+      //   if (!state) state = {};
 
-    //a client want to join
-    // if (result.method === 'join') {
-    //   const clientId = result.clientId;
-    //   const userName = result.userName;
+      //   state[score] = score;
+      //   games[gameId].state = state;
+      // }
+    });
+    connection.on('error', (err) => {
+      console.error(
+        'Socket encountered error: ',
+        err.message,
+        'Closing socket'
+      );
+      connect();
+    });
 
-    //   const gameId = result.gameId;
-    //   const game = games[gameId];
-    //   if (game.clients.length >= 3) {
-    //     //sorry max players reach
-    //     return;
-    //   }
-    //   // const color = { 0: 'Red', 1: 'Green', 2: 'Blue' }[game.clients.length];
-    //   game.clients.push({
-    //     clientId: clientId,
-    //     userName: userName,
-    //     score: 0,
-    //   });
-    //   //start the game
-    //   if (game.clients.length === 3) updateGameState();
-
-    //   const payLoad = {
-    //     method: 'join',
-    //     game: game,
-    //   };
-    //   //loop through all clients and tell them that people has joined
-    //   game.clients.forEach((c) => {
-    //     clients[c.clientId].connection.send(JSON.stringify(payLoad));
-    //   });
-    // }
-    //a user plays
-    // if (result.method === 'play') {
-    //   const gameId = result.gameId;
-    //   // const ballId = result.ballId;
-    //   const score = result.score;
-    //   let state = games[gameId].state;
-    //   if (!state) state = {};
-
-    //   state[score] = score;
-    //   games[gameId].state = state;
-    // }
+    connection.send(JSON.stringify(payLoad));
   });
-  connection.send(JSON.stringify(payLoad));
-});
-
+connect();
 function updateGameState() {
   //{"gameid", fasdfsf}
   for (const g of Object.keys(games)) {
