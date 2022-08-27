2b7bc958f7c23f08022c1f25e1177e78e15d291f
Abdullah Shehu <bigtulahi1@gmail.com>
2022-08-27 15:29:52 +0100

    more questions

--- Answers/Answers.js
+++ Answers/Answers.js
@@ -19,6 +19,74 @@ const wow = [
     word: 'COMIC FRIDAY',
     hint: 'Cool website filled with unfunny comics',
   },
+  {
+    word: 'BLACK PINK',
+    hint: 'Biggest female K-Pop group',
+  },
+  {
+    word: 'NARUTO',
+    hint: 'RASENGAN!!!',
+  },
+  {
+    word: 'TINUBU',
+    hint: 'is it for eba, garri, beans and dodo?',
+  },
+  {
+    word: 'JAZZ',
+    hint: 'music genre that originated from Black American communities',
+  },
+  {
+    word: 'GREEN GOBLIN',
+    hint: "Spider-Man's archenemy",
+  },
+  {
+    word: 'SPEEDOMETER',
+    hint: 'An important car part',
+  },
+  {
+    word: 'THE VOID',
+    hint: 'The name of this game 🤨',
+  },
+  {
+    word: 'THANOS',
+    hint: 'I am inevitable!!!',
+  },
+  {
+    word: 'SADIO MANE',
+    hint: 'Player to score the fastest hattrick ever',
+  },
+  {
+    word: 'TUNISIA',
+    hint: 'African country with the most valuable currency',
+  },
+  {
+    word: 'YEMEN',
+    hint: 'The only country in the world that starts with letter Y',
+  },
+  {
+    word: 'BERLIN',
+    hint: 'Capital of germany',
+  },
+  {
+    word: 'JAMES CHARLES',
+    hint: 'Hi Sisters!!💅🏻🏳️‍🌈',
+  },
+  {
+    word: 'ARES',
+    hint: 'Greek God',
+  },
+  {
+    word: 'JAFAR',
+    hint: 'Greek God',
+  },
+  {
+    word: 'MOTHER',
+    hint: "Your brother's wife's father-inlaw's wife is your...",
+  },
+  {
+    word: 'MUSHROOM',
+    hint: 'I am a type of room you can not enter or leave. What am I?',
+  },
 ];
 
 function shuffle(array) {
@@ -41,4 +109,4 @@ function shuffle(array) {
   return array;
 }
 
-module.exports.Ideas = shuffle(wow);
+module.exports.Ideas = shuffle(wow).slice(0, 5);
